cb59e5602dae29f57c11e31c07a993512abf82dd
tenahe <tenahemat@gmail.com>
2022-03-10 23:28:14 -1000

    introduction des tests auto, avec exeptions

--- tp2_absolute.py
+++ tp2_absolute.py
@@ -1,3 +1,19 @@
+import unittest
+
+class mytest(unittest.TestCase):
+    def test_a(self):
+        va=a(-3)
+        assertEquals(va,3)
+
+    def test_a(self):
+        va=a(-5)
+        assertEquals(va,5)
+
+if __name__=='__main__':
+    unittest.main()
+        
+
+
 def a(i):
     b=0
     if i<=0:
@@ -7,5 +23,6 @@ def a(i):
     return b
 
 i=-5
-b=a(i)
-print (b)
+c=a(i)
+print (c)
+
